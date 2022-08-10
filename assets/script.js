7ff37d946d4010fa6e056f63510ca8806e8f59a0
Juanchis <juanchismo10@gmail.com>
2022-08-09 23:13:02 -0300

    Creo la funcionalidad de encriptación

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,44 @@ const div = document.getElementById('right-side')
 function hiddeElement(element){
     element.style.display = 'none'
 }
-
-
+// encrypt
+function encrypt(str){
+    let lower = str.toLowerCase()
+    let chars = lower.split('')
+    let newString = ''
+    
+    for(let i = 0; i < chars.length; i++){
+        
+        switch(chars[i]){
+            case 'e':
+                chars[i] = 'enter'
+                break;
+            case 'i':
+                chars[i] = 'imes'
+                break;
+            case 'a':
+                chars[i] = 'ai'
+                break;
+            case 'o':
+                chars[i] = 'ober'
+                break;
+            case 'u':
+                chars[i] = 'ufat'
+                break;
+            }
+            newString += chars[i]   
+        
+    }
+    return newString
+}
 
 button.addEventListener('click', function(){
     
     if(input.value != ""){
         hiddeElement(img)
         hiddeElement(p)
-
-        div.innerHTML = `<p class="text_content"> ${input.value}</p>`
+        
+        div.innerHTML = `<p class="text_content"> ${encrypt(input.value)}</p>`
         input.value = ""  
     }
-
 })
